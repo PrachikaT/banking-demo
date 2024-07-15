14852afb201b393ec9c576a9680ee0a438ebb228
PrachikaT <instantprachu@gmail.com>
2024-07-15 20:23:30 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
 }
 
 # Creating a VPC
@@ -45,7 +45,7 @@ resource "aws_route_table" "proj-rt" {
 resource "aws_subnet" "proj-subnet" {
   vpc_id            = aws_vpc.proj-vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "ap-southeast-1"
   tags = {
     Name = "subnet1"
   }
@@ -116,10 +116,10 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
-  ami               = "ami-0c2af51e265bd5e0e"
+  ami               = "ami-0497a974f8d5dcef8"
   instance_type     = "t2.micro"
-  availability_zone = "ap-south-1b"
-  key_name          = "rahulnew.kp"
+  availability_zone = "ap-southeast-1"
+  key_name          = "bank-app"
   network_interface {
     device_index          = 0
     network_interface_id  = aws_network_interface.proj-ni.id
